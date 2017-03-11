f538ab197a9e2c2333ee76f3586afa0326268f92
Richer Balázs <richer.balazs@gmail.com>
2017-03-11 17:23:18 +0100

    Wire up sequelize with sqllite.

--- server.js
+++ server.js
@@ -2,6 +2,34 @@
 
 const express = require('express');
 
+let Sequelize = require('sequelize');
+
+let sequelize = new Sequelize('main', '', '', {
+  host: 'localhost',
+  dialect: 'sqlite',
+  
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  },
+  
+  storage: 'db.db'
+});
+
+var Word = sequelize.define('word', {
+  foreign: {
+    type: Sequelize.STRING,
+    field: 'foreign'
+  },
+  plain: {
+    type: Sequelize.STRING,
+    field: 'plain'
+  }
+}, {
+  freezeTableName: true
+});
+
 const PORT = 8080;
 
 const app = express();
@@ -15,18 +43,26 @@ app.set('views', __dirname + '/views');
 
 app.use(express.static('public'));
 app.disable('view cache');
-app.get('*', function (req, res) {
-  var person = {
-    firstName: "Christophe",
-    lastName: "Coenraets",
-    blogURL: "http://coenraets.org"
-  };
-  res.render('index',
-    {
-      message: "Hello World!",
-      items: ["name", "two", "three"]
-    }
-  );
+app.get('/', function (req, res) {
+  Word.findAll().then(function (words) {
+    res.render('index',
+      {
+        message: "Szavak",
+        words: words
+      }
+    );
+  })
+});
+
+app.get('/szotar', function (req, res) {
+  Word.findAll().then(function (words) {
+    res.render('index',
+      {
+        message: "szótár",
+        words: words
+      }
+    );
+  })
 });
 
 app.listen(process.env.PORT || PORT);
