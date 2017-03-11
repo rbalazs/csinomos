6e6a6b4cce915af2550679ddca526ea2cfb7193a
Richer Balázs <richer.balazs@gmail.com>
2017-03-11 19:02:58 +0100

    Word list can open for editing.

--- public/controller/PageController.js
+++ public/controller/PageController.js
@@ -17,12 +17,32 @@ let callback = function ($) {
   /**
    * @constructor
    */
-  return function PageController () {
+  return function PageController() {
     /**
      * Starts up the application.
      */
     this.execute = function () {
+      let opened = undefined;
       
+      $('.word-item').click(function () {
+        if (opened) {
+          return;
+        }
+        
+        let $item = $(this);
+        
+        opened = $item;
+        
+        let $plain = $item.children('.plain');
+        let $foreign = $item.children('.foreign');
+        
+        let id = $item.data('id');
+        let foreign = $foreign.text().trim();
+        let plain = $plain.text().trim();
+        
+        $foreign.replaceWith($('<input>').val(foreign));
+        $plain.replaceWith($('<input>').val(plain));
+      });
     };
   };
 };
