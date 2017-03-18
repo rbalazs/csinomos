816c7d779dbaab1305c73155af29aa28f72fe5ee
Richer Balázs <richer.balazs@gmail.com>
2017-03-18 08:44:27 +0100

    Restructure controller code.

--- src/controller/IndexController.js
+++ src/controller/IndexController.js
@@ -45,20 +45,25 @@ let IndexController = function () {
   this.execute = function (req, res, apiKeyService, Word, googleOauth2UserService) {
     apiKeyService.getKey().then(function (key) {
       let clientKeys = JSON.parse(key);
+      
       googleOauth2UserService.initClient(clientKeys);
-      if (!req.session.tokens) {
-        if (req.query.code && req.session.code != req.query.code) {
-          req.session.code = req.query.code;
-        } else {
-          res.redirect(googleOauth2UserService.getAuthUrl(clientKeys));
-          return;
-        }
-        googleOauth2UserService.fetchTokens(req.session.code).then(function (tokens) {
-          req.session.tokens = tokens;
-          self.sendResponse(googleOauth2UserService, req, Word, res);
-        });
+      
+      if (req.session.tokens) {
+        self.sendResponse(googleOauth2UserService, req, Word, res);
+        return;
       }
-      self.sendResponse(googleOauth2UserService, req, Word, res);
+      
+      if (req.query.code && req.session.code != req.query.code) {
+        req.session.code = req.query.code;
+      } else {
+        res.redirect(googleOauth2UserService.getAuthUrl(clientKeys));
+        return;
+      }
+      
+      googleOauth2UserService.fetchTokens(req.session.code).then(function (tokens) {
+        req.session.tokens = tokens;
+        self.sendResponse(googleOauth2UserService, req, Word, res);
+      });
     });
   };
 };
