1a68a46f02a3d833a5888a44a4b984471456b2fd
Richer Balázs <richer.balazs@gmail.com>
2017-03-25 17:52:53 +0100

    Rewrite index controller to Promise chain

--- server.js
+++ server.js
@@ -26,19 +26,17 @@ app.use(session({
 }));
 
 router.get('/szotar', function (req, res) {
-  
   let APIKeyService = require('./src/service/APIKeyService');
-  let apiKeyService = new APIKeyService();
-  
+  let IndexController = require('./src/controller/IndexController');
   let GoogleOauth2UserService = require('./src/service/GoogleOauth2UserService');
+  
+  let apiKeyService = new APIKeyService();
   let googleOauth2UserService = new GoogleOauth2UserService();
   
-  let IndexController = require('./src/controller/IndexController');
-  let controller = new IndexController();
+  let controller = new IndexController(req, res, apiKeyService, Word, googleOauth2UserService);
   
-  controller.execute(req, res, apiKeyService, Word, googleOauth2UserService);
+  controller.execute();
 });
-
 
 router.get('/auth', function (req, res) {
   let AuthController = require('./src/controller/AuthController');
@@ -46,8 +44,7 @@ router.get('/auth', function (req, res) {
   controller.execute(req, res);
 });
 
-router.route('/word/:word_id')
-.put(function (req, res) {
+router.route('/word/:word_id').put(function (req, res) {
   Word.findById(req.params.word_id).then(function (word) {
     word[req.body.key] = req.body.value;
     word.save().then(function () {
--- src/controller/IndexController.js
+++ src/controller/IndexController.js
@@ -1,50 +1,112 @@
-"use strict";
+'use strict';
 
 /**
  * Applications main controller.
  *
+ * @param {IncomingMessage} request
+ * @param {ServerResponse} response
+ * @param {APIKeyService} apiKeyService
+ * @param {Word} Word
+ * @param {GoogleOauth2UserService} googleOauth2UserService
+ *
  * @constructor
  */
-let IndexController = function () {
+let IndexController = function (request, response, apiKeyService, Word, googleOauth2UserService) {
   /**
    * @type {IndexController}
    */
   let self = this;
   
   /**
-   * Handles index page action.
-   *
-   * @param {Object} request
-   * @param {Object} response
-   * @param {APIKeyService} apiKeyService
-   * @param {Word} Word
-   * @param {GoogleOauth2UserService} googleOauth2UserService
+   * @type {IncomingMessage}
    */
-  this.execute = function (request, response, apiKeyService, Word, googleOauth2UserService) {
-    apiKeyService.getKey().then(function (key) {
-      let clientKeys = JSON.parse(key);
-      
-      googleOauth2UserService.initClient(clientKeys);
-      
-      if (!request.session.code) {
-        response.redirect(googleOauth2UserService.getAuthUrl(clientKeys));
-        return;
-      }
-      
-      googleOauth2UserService.fetchTokens(request.session.code, request.session.tokens).then(function (tokens) {
-        request.session.tokens = tokens;
-        googleOauth2UserService.fetchProfile(request.session.tokens).then(function (profile) {
-          Word.findAll().then(function (words) {
-            response.render('index',
-              {
-                message: 'Szavak',
-                words: words,
-                profileDisplayName: profile.displayName
-              }
-            );
-          })
-        });
-      });
+  this.request = request;
+  
+  /**
+   * @type {ServerResponse}
+   */
+  this.response = response;
+  
+  /**
+   * @type {APIKeyService}
+   */
+  this.apiKeyService = apiKeyService;
+  
+  /**
+   * @type {Word}
+   */
+  this.Word = Word;
+  
+  /**
+   * @type {GoogleOauth2UserService}
+   */
+  this.googleOauth2UserService = googleOauth2UserService;
+  
+  /**
+   * @type {string}
+   */
+  this.profileName = null;
+  
+  /**
+   * @param {string} profile
+   *
+   * @return {Promise}
+   */
+  this.fetchWords = function (profile) {
+    self.profileName = profile.displayName;
+    
+    return self.Word.findAll()
+  };
+  
+  /**
+   * @param {string} tokens
+   *
+   * @return {Promise}
+   */
+  this.fetchGooglePlusProfile = function (tokens) {
+    self.request.session.tokens = tokens;
+    return self.googleOauth2UserService.fetchProfile(self.request.session.tokens);
+  };
+  
+  /**
+   * @param {string} key
+   *
+   * @return {Promise}
+   */
+  this.fetchOauthTokens = function (key) {
+    let clientKeys = JSON.parse(key);
+    
+    self.googleOauth2UserService.initClient(clientKeys);
+    
+    if (!self.request.session.code) {
+      self.response.redirect(self.googleOauth2UserService.getAuthUrl(clientKeys));
+    }
+    
+    return self.googleOauth2UserService.fetchTokens(
+      self.request.session.code,
+      self.request.session.tokens
+    );
+  };
+  
+  /**
+   * Executes index apge action.
+   */
+  this.execute = function () {
+    self.apiKeyService.getKey()
+    .then(self.fetchOauthTokens)
+    .then(self.fetchGooglePlusProfile)
+    .then(self.fetchWords)
+    .then((words) => {
+      response.render('index',
+        {
+          message: 'Szavak',
+          words: words,
+          profileDisplayName: self.profileName
+        }
+      )
+    })
+    .catch(function (e) {
+      console.log(e);
     });
   };
 };
